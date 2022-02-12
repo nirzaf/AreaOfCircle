d2639821ed647afde6b4eb0d3f30f2f2ff6f14e2
nirzaf <mfmfazrin1986@gmail.com>
2022-02-12 11:16:35 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class Circle
+{
+    private const double pie = 3.14;
+
+    public double GetArea(double radius)
+    {
+        return radius * radius * pie;
+    }
+
+    public double GetCircumference(double radius)
+    {
+        return 2 * pie * radius;
+    }
+}
